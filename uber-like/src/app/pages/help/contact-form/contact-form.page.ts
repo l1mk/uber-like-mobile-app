5fe9ef0055e4eaa7d5701e93df6c9abc6b59cf74
l1mk <jhonxlink@hotmail.com>
2021-07-13 08:37:07 -0500

    added contact page form

--- uber-like/src/app/pages/help/contact-form/contact-form.page.ts
+++ uber-like/src/app/pages/help/contact-form/contact-form.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -10,6 +11,10 @@ export class ContactFormPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  submit(fLogin: NgForm){
+    console.log(fLogin.valid);
   }
 
 }
